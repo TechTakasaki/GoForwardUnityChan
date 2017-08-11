d31bfd44b8f346f11bc4f1d289433a8b2b1e385b
布施和隆 <firebom.sns@gmail.com>
2017-08-11 18:20:33 +0900

    よろしくお願いします。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour
+{
+    
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    private float groundLevel = -3.0f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        //障害物に衝突した場合（追加）
+        if (col.gameObject.tag == "GroundTag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+
+        if (col.gameObject.tag == "CubeTag")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
